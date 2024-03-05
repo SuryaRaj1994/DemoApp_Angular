85b0a7932ea92c1478a26af199193759de527ad1
Surya <suryagr21@gmail.com>
2024-03-05 15:30:14 +0530

    some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,HeaderComponent],
+  imports: [RouterOutlet,NavbarComponent,HeaderComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
